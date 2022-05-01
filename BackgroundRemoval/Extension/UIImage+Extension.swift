eef789c5591885f1c7cb60f29bbf3d18ad066a68
Emre Celik <emrecelikk97@gmail.com>
2022-05-02 00:59:50 +0300

    u2netp model added to view
    - info.plist camera usage description added
    - UIImage extensions added

---
+++ BackgroundRemoval/Extension/UIImage+Extension.swift
@@ -0,0 +1,77 @@
+//
+//  UIImage+Extension.swift
+//  BackgroundRemoval
+//
+//  Created by Emre Çelik on 2.05.2022.
+//
+
+import UIKit
+import VideoToolbox
+
+extension UIImage {
+    func resizedCroppedImage(newSize: CGSize) -> UIImage? {
+        var ratio: CGFloat = 0
+        var delta: CGFloat = 0
+        var offset = CGPoint.zero
+        if self.size.width > self.size.height {
+            ratio = newSize.width / self.size.width
+            delta = (ratio * self.size.width) - (ratio * self.size.height)
+            offset = CGPoint(x: delta / 2, y: 0)
+        } else {
+            ratio = newSize.width / self.size.height
+            delta = (ratio * self.size.height) - (ratio * self.size.width)
+            offset = CGPoint(x: 0, y: delta / 2)
+        }
+        let clipRect = CGRect(x: -offset.x,
+                              y: -offset.y,
+                              width: (ratio * self.size.width) + delta,
+                              height: (ratio * self.size.height) + delta)
+        UIGraphicsBeginImageContextWithOptions(newSize, true, 0.0)
+        UIRectClip(clipRect)
+        self.draw(in: clipRect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    public func pixelBuffer() -> CVPixelBuffer? {
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+             kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                         Int(self.size.width),
+                                         Int(self.size.height),
+                                         kCVPixelFormatType_32ARGB,
+                                         attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else { return nil }
+
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData,
+                                width: Int(self.size.width),
+                                height: Int(self.size.height),
+                                bitsPerComponent: 8,
+                                bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!),
+                                space: rgbColorSpace,
+                                bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+
+        UIGraphicsPushContext(context!)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+
+        return pixelBuffer
+    }
+    
+    public convenience init?(pixelBuffer: CVPixelBuffer) {
+        var cgImage: CGImage?
+        VTCreateCGImageFromCVPixelBuffer(pixelBuffer, options: nil, imageOut: &cgImage)
+        guard let cgImage = cgImage else { return nil }
+        self.init(cgImage: cgImage)
+    }
+}
--- BackgroundRemoval/ViewController.swift
+++ BackgroundRemoval/ViewController.swift
@@ -6,14 +6,80 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var captureView: UIView!
+    @IBOutlet private weak var filterImageView: UIImageView!
+    
+    // MARK: Camera Capture Variables
+    var deviceInput: AVCaptureDeviceInput!
+    let session = AVCaptureSession()
+    let videoQueue = DispatchQueue(label: "video",
+                                   qos: .userInteractive,
+                                   attributes: [],
+                                   autoreleaseFrequency: .workItem)
+    let videoDataOutput = AVCaptureVideoDataOutput()
+    var rootLayer: CALayer! = nil
+    var previewLayer: AVCaptureVideoPreviewLayer! = nil
+    
+    // MARK: U2Net Model
+    var model: u2netp!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        model = try? u2netp.init()
+        setupVideoCaptureAndLayers()
+        session.startRunning()
+    }
+    
+    func setupVideoCaptureAndLayers() {
+        let videoDevice = AVCaptureDevice.default(for: .video)
+        do { deviceInput = try AVCaptureDeviceInput(device:videoDevice!) } catch { return }
+        session.beginConfiguration()
+        session.sessionPreset = .vga640x480
+        if session.canAddOutput(videoDataOutput) && session.canAddInput(deviceInput) {
+            session.addInput(deviceInput)
+            session.addOutput(videoDataOutput)
+            videoDataOutput.videoSettings = [
+                String(kCVPixelBufferPixelFormatTypeKey): NSNumber(value: kCVPixelFormatType_32BGRA)
+            ]
+            videoDataOutput.setSampleBufferDelegate(self, queue: videoQueue)
+        } else { return }
+        
+        session.commitConfiguration()
+        
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        
+        rootLayer = captureView.layer
+        rootLayer.position = CGPoint(x: rootLayer.bounds.midX, y: rootLayer.bounds.midY)
+        
+        previewLayer.frame = rootLayer.bounds
+        rootLayer.addSublayer(previewLayer)
+    }
+    
+    func getFilterImage(sampleBuffer: CMSampleBuffer) -> UIImage? {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return nil }
+        let ciimage = CIImage(cvPixelBuffer: pixelBuffer).oriented(.right)
+        guard let croppedImage = UIImage(ciImage: ciimage).resizedCroppedImage(newSize: CGSize(width: 320, height: 320)) else { return nil }
+        guard let buffer = croppedImage.pixelBuffer() else { return nil }
+        let result = try? self.model?.prediction(in_0: buffer)
+        guard let out_p1 = result?.out_p0, let output = UIImage(pixelBuffer: out_p1) else { return nil }
+        return output
     }
 
 
 }
 
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let filterImage = getFilterImage(sampleBuffer: sampleBuffer) else { return }
+        DispatchQueue.main.async { [weak self] in
+            self?.filterImageView.image = filterImage
+        }
+    }
+}
