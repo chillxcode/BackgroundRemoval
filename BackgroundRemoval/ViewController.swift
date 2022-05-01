c134f7d7d2e42f14e64bc5d70c8dfd8f8515600b
Emre Celik <emrecelikk97@gmail.com>
2022-05-02 00:03:36 +0300

    project init

---
+++ BackgroundRemoval/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroundRemoval
+//
+//  Created by Emre Çelik on 2.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
